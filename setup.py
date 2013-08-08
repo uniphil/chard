11433d3c43752d6862e03bde5505e1a4698847c7
Philip Schleihauf <uniphil@gmail.com>
2013-08-08 11:14:32 -0400

    oldie support

--- setup.py
+++ setup.py
@@ -8,19 +8,22 @@
     :license: dwtfyw. and i like coffee.
 """
 
+from sys import version_info
 from setuptools import setup
 
 
-with open('readme.md') as readme:
-    setup(
-        name='chard',
-        version='0',
-        author='Phil Schleihauf',
-        author_email='uniphil@gmail.com',
-        url='https://github.com/uniphil/chard',
-        license='blah blah blah',
-        description='MongoDB Introspection-ish. ...',
-        long_description=readme.read(),
-        install_requires=[],
-        py_modules=['chard'],
-    )
+with open('readme.md') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='chard',
+    version='0',
+    author='Phil Schleihauf',
+    author_email='uniphil@gmail.com',
+    url='https://github.com/uniphil/chard',
+    license='blah blah blah',
+    description='MongoDB Introspection-ish. ...',
+    long_description=readme,
+    install_requires=[] if version_info >= (2, 7, 0) else ['ordereddict'],
+    py_modules=['chard'],
+)
